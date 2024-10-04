8c57243e6fb3b4b833644126e51790df9e3aecb2
Victor kichwen <victor.kichwen@student.moringaschool.com>
2024-10-04 21:14:12 +0300

    complete lab 13

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+// const farmAnimals = ['cow horse sheep pig chicken'];
+// const animalSounds= ['moo neck mee oink cooks']
+// console.log(cowSound);  // Output: Woof
+// console.log(horseSound);  // Output: Meow
+// console.log(sheepSound); // Output: Quack
+// console.log(pigSound);  // Output: Moo
+// console.log(chickenSound); // Output: Roar
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
-};
 
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
-};
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// const muppet = {
+//   muppetName: 'Miss Piggy',
+//   color: 'pink',
+//   song: 'Never Before, Never Again',
+//   job: 'Cast member of The Muppet Show',
+//   partner: 'Kermit'
+// };
+
+// const nestedMuppet = {
+//   nestedName: 'Kermit',
+//   nestedColor: 'green',
+//   album: {
+//     theMuppetMovie: {
+//       song1: 'Rainbow Connection',
+//       song2: 'Moving Right Along',
+//       song3: 'Never Before, Never Again',
+//       song4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   nestedJob: 'Host of The Muppet Show',
+//   nestedPartner: 'Miss Piggy'
+// };
+// // index.js
+
+// // Array of animal sounds
+// // const animalSounds = ['Woof', 'Meow', 'Quack', 'Moo', 'Roar'];
+
+// // // Destructuring the array
+// // const [dogSound, catSound, duckSound, cowSound, lionSound] = animalSounds;
+
+// // console.log(dogSound);  // Output: Woof
+// // console.log(catSound);  // Output: Meow
+// // console.log(duckSound); // Output: Quack
+// // console.log(cowSound);  // Output: Moo
+// // console.log(lionSound); // Output: Roar
+
+// index.js
+
+// Array of farm animal sounds
+// const farmAnimals = ['Moo', 'Neigh', 'Baa', 'Oink', 'Cluck'];
+
+// // Destructuring the array to declare individual animal sounds
+// const [cowSound, horseSound, sheepSound, pigSound, chickenSound] = farmAnimals;
+
+// console.log(cowSound);     // Output: Moo
+// console.log(horseSound);   // Output: Neigh
+// console.log(sheepSound);   // Output: Baa
+// console.log(pigSound);     // Output: Oink
+// console.log(chickenSound);  // Output: Cluck
 
 // Strings
 
@@ -46,3 +81,274 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+
+// index.js
+
+// 1) String: uses destructuring to declare five animal sounds
+// const animalSounds = ['Moo', 'Neigh', 'Baa', 'Oink', 'Cluck'];
+// const [cowSound, horseSound, sheepSound, pigSound, chickenSound] = animalSounds;
+
+// console.log(cowSound);     // Output: Moo
+// console.log(horseSound);   // Output: Neigh
+// console.log(sheepSound);   // Output: Baa
+// console.log(pigSound);     // Output: Oink
+// console.log(chickenSound);  // Output: Cluck
+
+// 2) String: uses destructuring to declare the four traditional animal names
+// const animalNames = ['Cow', 'Horse', 'Sheep', 'Pig'];
+// const [firstAnimal, secondAnimal, thirdAnimal, fourthAnimal] = animalNames;
+
+// console.log(firstAnimal);   // Output: Cow
+// console.log(secondAnimal);  // Output: Horse
+// console.log(thirdAnimal);   // Output: Sheep
+// console.log(fourthAnimal);  // Output: Pig
+
+// // 3) String: uses destructuring to declare the three traditional animal colors
+// const animalColors = ['Black', 'White', 'Brown'];
+// const [firstColor, secondColor, thirdColor] = animalColors;
+
+// console.log(firstColor);    // Output: Black
+// console.log(secondColor);   // Output: White
+// console.log(thirdColor);    // Output: Brown
+
+// 4) Array: uses destructuring to declare the seven traditional rainbow color variables using the color names
+// const rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+// const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+// console.log(red);    // Output: Red
+// console.log(orange); // Output: Orange
+// console.log(yellow); // Output: Yellow
+// console.log(green);  // Output: Green
+// console.log(blue);   // Output: Blue
+// console.log(indigo); // Output: Indigo
+// console.log(violet); // Output: Violet
+
+// 5) Array: uses destructuring to declare six rainbow color variables using initials
+// 
+
+// index.js
+
+// 1) String: uses destructuring to declare five animal sounds
+// const animalSounds = ['Moo', 'Neigh', 'Baa', 'Oink', 'Cluck'];
+// const [cowSound, horseSound, sheepSound, pigSound, chickenSound] = animalSounds;
+
+// console.log(cowSound);     // Output: Moo
+// console.log(horseSound);   // Output: Neigh
+// console.log(sheepSound);   // Output: Baa
+// console.log(pigSound);     // Output: Oink
+// console.log(chickenSound);  // Output: Cluck
+
+// // 2) String: uses destructuring to declare the four traditional animal names
+// const animalNames = ['Cow', 'Horse', 'Sheep', 'Pig'];
+// const [firstAnimal, secondAnimal, thirdAnimal, fourthAnimal] = animalNames;
+
+// console.log(firstAnimal);   // Output: Cow
+// console.log(secondAnimal);  // Output: Horse
+// console.log(thirdAnimal);   // Output: Sheep
+// console.log(fourthAnimal);  // Output: Pig
+
+// // 3) String: uses destructuring to declare the three traditional animal colors
+// const animalColors = ['Black', 'White', 'Brown'];
+// const [firstColor, secondColor, thirdColor] = animalColors;
+
+// console.log(firstColor);    // Output: Black
+// console.log(secondColor);   // Output: White
+// console.log(thirdColor);    // Output: Brown
+
+// // 4) Array: uses destructuring to declare the seven traditional rainbow color variables using the color names
+// const rainbowColors = ['red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+// const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+// console.log(red);    // Output: Red
+// console.log(orange); // Output: Orange
+// console.log(yellow); // Output: Yellow
+// console.log(green);  // Output: Green
+// console.log(blue);   // Output: Blue
+// console.log(indigo); // Output: Indigo
+// console.log(violet); // Output: Violet
+
+// // 5) Array: uses destructuring to declare six rainbow color variables using initials
+// const rainbowInitials = ['R', 'O', 'Y', 'G', 'B', 'I'];
+// const [r, o, y, g, b, i] = rainbowInitials;
+
+// console.log(r); // Output: R
+// console.log(o); // Output: O
+// console.log(y); // Output: Y
+// console.log(g); // Output: G
+// console.log(b); // Output: B
+// console.log(i); // Output: I
+
+// // 6) Array: uses destructuring to declare Indigo using indg
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'];
+// const [, , , , , indg] = colors; // Skip to get Indigo
+
+// console.log(indg); // Output: Indigo
+
+// // 7) Object: uses destructuring to assign all appropriate variables using the keys as the variable names
+// const animalProperties = {
+//     sound: 'Moo',
+//     name: 'Cow',
+//     color: 'Black',
+//     age: 5
+// };
+// const { sound, name, color, age } = animalProperties;
+
+// console.log(sound); // Output: Moo
+// console.log(name);  // Output: Cow
+// console.log(color); // Output: Black
+// console.log(age);   // Output: 5
+
+// // 8) Object: uses destructuring to assign songs 2 and 4, and Kermit's job and partner
+// const muppetDetails = {
+//     songs: ['Rainbow Connection', 'Moving Right Along', 'The Muppet Show Theme', 'Bein’ Green'],
+//     kermit: {
+//         job: 'Singer',
+//         partner: 'Miss Piggy'
+//     }
+// };
+// const [, song2, , song4] = muppetDetails.songs; // Songs 2 and 4
+// const { job, partner } = muppetDetails.kermit; // Kermit's job and partner
+
+// 1) Animal Sounds
+// 
+
+// 1) Animal Sounds
+// 
+
+// 1) String: uses destructuring to declare five animal sounds
+// const animalSounds = ['Moo', 'Neigh', 'Baa', 'Oink', 'Cluck'];
+// const [cowSound, horseSound, sheepSound, pigSound, chickenSound] = animalSounds;
+
+// console.log(cowSound);     // Output: Moo
+// console.log(horseSound);   // Output: Neigh
+// console.log(sheepSound);   // Output: Baa
+// console.log(pigSound);     // Output: Oink
+// console.log(chickenSound);  // Output: Cluck
+
+// // 2) String: uses destructuring to declare the four traditional animal names
+// const animalNames = ['Cow', 'Horse', 'Sheep', 'Pig'];
+// const [firstAnimal, secondAnimal, thirdAnimal, fourthAnimal] = animalNames;
+
+// console.log(firstAnimal);   // Output: Cow
+// console.log(secondAnimal);  // Output: Horse
+// console.log(thirdAnimal);   // Output: Sheep
+// console.log(fourthAnimal);  // Output: Pig
+
+// // 3) String: uses destructuring to declare the three traditional animal colors
+// const animalColors = ['Black', 'White', 'Brown'];
+// const [firstColor, secondColor, thirdColor] = animalColors;
+
+// console.log(firstColor);    // Output: Black
+// console.log(secondColor);   // Output: White
+// console.log(thirdColor);    // Output: Brown
+
+// // 4) Array: uses destructuring to declare the seven traditional rainbow color variables using the color names
+// const rainbowColors = ['red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+// const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+// console.log(red);    // Output: red
+// console.log(orange); // Output: Orange
+// console.log(yellow); // Output: Yellow
+// console.log(green);  // Output: Green
+// console.log(blue);   // Output: Blue
+// console.log(indigo); // Output: Indigo
+// console.log(violet); // Output: Violet
+
+// // 5) Array: uses destructuring to declare six rainbow color variables using initials
+// const rainbowInitials = ['R', 'O', 'Y', 'G', 'B', 'I'];
+// const [r, o, y, g, b, i] = rainbowInitials;
+
+// console.log(r); // Output: R
+// console.log(o); // Output: O
+// console.log(y); // Output: Y
+// console.log(g); // Output: G
+// console.log(b); // Output: B
+// console.log(i); // Output: I
+
+// // 6) Array: uses destructuring to declare Indigo using indg
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'];
+// const [, , , , , indg] = colors; // Skip to get Indigo
+
+// console.log(indg); // Output: indigo
+
+// // 7) Object: uses destructuring to assign all appropriate variables using the keys as the variable names
+// const animalProperties = {
+//     sound: 'Moo',
+//     name: 'Cow',
+//     color: 'Black',
+//     age: 5
+// };
+// const { sound, name, color, age } = animalProperties;
+
+// console.log(sound); // Output: Moo
+// console.log(name);  // Output: Cow
+// console.log(color); // Output: Black
+// console.log(age);   // Output: 5
+
+// // 8) Object: uses destructuring to assign songs 2 and 4, and Kermit's job and partner
+// // c
+// const nestedJob = {
+//   songs: [
+//       'Rainbow Connection', 
+//       'Moving Right Along', 
+//       'The Muppet Show Theme', 
+//       'I Hope That Something Better Comes Along'
+//   ],
+//   kermit: {
+//       job: 'Singer',
+//       partner: 'Miss Piggy'
+//   }
+// };
+
+// // Destructure songs 2 and 4
+// const [, song2, , song4] = nestedJob.songs; // Songs 2 and 4
+// // Destructure Kermit's job and partner
+// const { job, partner } = nestedJob.kermit; // Kermit's job and partner
+
+// console.log(song2); // Output: Moving Right Along
+// console.log(song4); // Output: I Hope That Something Better Comes Along
+// console.log(job);   // Output: Singer
+// console.log(partner); // Output: Miss Piggy
+ 
+// // Example Assertions
+// assert.strictEqual(job, 'Singer'); // Checks if job is 'Singer'
+// assert.strictEqual(partner, 'Miss Piggy'); // Checks if partner is 'Miss Piggy'
+
+{const farmAnimals = 'cow horse sheep pig chicken';
+
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    
+    const muppet = {
+      muppetName: 'Miss Piggy',
+      color: 'pink',
+      song: 'Never Before, Never Again',
+      job: 'Cast member of The Muppet Show',
+      partner: 'Kermit'
+    };
+    
+    const nestedMuppet = {
+      nestedName: 'Kermit',
+      nestedColor: 'green',
+      album: {
+        theMuppetMovie: {
+          song1: 'Rainbow Connection',
+          song2: 'Moving Right Along',
+          song3: 'Never Before, Never Again',
+          song4: 'I Hope That Something Better Comes Along',
+        },
+      },
+      nestedJob: 'Host of The Muppet Show',
+      nestedPartner: 'Miss Piggy'
+    };
+    
+    
+    const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ")
+    const [bessie, , dolly, babe, little] = farmAnimals.split(" ")
+    const [blackAndWhite, , black, pink, ] = farmAnimals.split(" ")
+    const [red, orange, yellow, green, blue, indigo, violet] = colors
+    const [r, o, y, g, b, , v] = colors
+    const [ , , , , , indg, ] = colors
+    const {muppetName, color, song, job, partner} = muppet
+    const {song2, song4} = nestedMuppet.album.theMuppetMovie
+    const { nestedJob, nestedPartner} = nestedMuppet}
---
+++ tok.js
@@ -0,0 +1,7 @@
+let money = 0
+if (money < 50){
+    console.log(money) 
+count++
+};
+
+
